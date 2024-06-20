70e4ec2acf1464394aef6519359398eb89e1a7a6
Hitesh Patil <143607470+hiteshpatil2005@users.noreply.github.com>
2024-06-20 21:54:35 +0530

    Create Queue_operation.c

---
+++ C /Queue/Queue_operation.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAXSIZE 20
+
+void enqueue();
+void dequeue();
+int queue[MAXSIZE];
+int front=-1,rear=-1;
+
+int main()
+{
+    int ch;
+   do{
+       printf("\n1.Enqueue.\n2.Dequeue.\n3.Exit.\nEnter the the number of operation that you want to perform:");
+    scanf("%d",&ch);
+    switch(ch)
+      {
+        case 1:enqueue();
+               break;
+        case 2:dequeue();
+               break;
+        case 3:exit(0);
+               break;
+        default: printf("Enter the valid operation number.\n");
+                 break;
+      }
+     }while(ch!=3);
+    return 0;
+}
+void enqueue()
+{
+    int a;
+    if(rear==MAXSIZE-1)
+    {
+        printf("The queue is overflow");
+    }
+    else
+    {
+            printf("\nEnter the number that you want to insert:\n");
+            scanf("%d",&a);
+            rear++;
+            queue[rear]=a;
+            if(front==-1)
+            {
+                front=0;
+            }
+    }
+}
+void dequeue()
+{
+    if(front==-1||front>rear)
+    {
+        printf("queue is underflow\n");
+    }
+    else
+    {
+        printf("\nNumber which is deleted from the queue is:%d\n",queue[front]);
+        front++;
+    }
+}
